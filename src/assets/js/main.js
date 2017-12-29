2c5539d2b5389563275a3cbb5af19494a3abc7a2
Maeva NAP <maevanap.contact@gmail.com>
2017-12-30 00:49:45 +0100

    separate the js files

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,128 +1,62 @@
-// Main object to represents the user
-let character = {
-  pseudo: 'My Pseudo',
-  avatar: '',
-  class: '',
-  level: 1,
-  experience: 0,
-  // Get an avatar name
-  getAvatar: function(avatarName) {
-    this.avatar = avatarName;
-  },
-  // Get a class name
-  getClass: function(className) {
-    this.class = className;
-  },
-  debugObject: function() {
-    console.log(this.pseudo);
-    console.log(this.avatar);
-    console.log(this.class);
-    console.log(this.level);
-    console.log(this.experience);
-  }
-}
-
-
-// Character Creation Page
-let characterCreation = {
-  getAvatar: function() {
-    const AVATAR_CHOICE = document.getElementById('avatar-choice');
-    const MAIN_AVATAR = document.getElementById('main-avatar');
-    let avatarItem = document.getElementsByClassName('avatar-item');
-
-    AVATAR_CHOICE.addEventListener('click', function(event) {
-      let elementClicked = event.target;
-      switch(elementClicked.id) {
-        case 'avatar-ape':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_ape.png');
-          character.avatar = 'ape';
-        break;
-        case 'avatar-dragon':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_dragon.png');
-          character.avatar = 'dragon';
-        break;
-        case 'avatar-elf-f':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_f_elf.png');
-          character.avatar = 'elf-f';
-        break;
-        case 'avatar-human-f':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_f_human.png');
-          character.avatar = 'human-f';
-        break;
-        case 'avatar-orc-f':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_f_orc.png');
-          character.avatar = 'orc-f';
-        break;
-        case 'avatar-elf-m':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_m_elf.png');
-          character.avatar = 'elf-m';
-        break;
-        case 'avatar-human-m':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_m_human.png');
-          character.avatar = 'human-m';
-        break;
-        case 'avatar-orc-m':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_m_orc.png');
-          character.avatar = 'orc-m';
-        break;
-        case 'avatar-panda':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_panda.png');
-          character.avatar = 'panda';
-        break;
-        case 'avatar-unicorn':
-          MAIN_AVATAR.setAttribute('src', 'assets/images/avatars/todo_unicorn.png');
-          character.avatar = 'unicorn';
-        break;
-      }
-      // Remove all the border colors
-      for (let i = 0; i < avatarItem.length; i++) {
-        avatarItem[i].style.borderColor = '#93928e';
-      }
-      // Set the border color to the selected avatar
-      elementClicked.style.borderColor = '#e5af29';
-      character.debugObject();
-    });
-  },
-  getClass: function() {
-    const CLASS_CHOICE = document.getElementById('class-choice');
-    let classItem = document.getElementsByClassName('class-item');
-
-    CLASS_CHOICE.addEventListener('click', function(event) {
-      let elementClicked = event.target;
-
-      switch(elementClicked.id) {
-        case 'class-mage':
-          character.class = 'mage';
-        break;
-        case 'class-rogue':
-          character.class = 'rogue';
-        break;
-        case 'class-warrior':
-          character.class = 'warrior';
-        break;
-      }
-
-      // Remove all the border colors
-      for (let i = 0; i < classItem.length; i++) {
-        classItem[i].style.borderColor = '#93928e';
-      }
-      // Set the border color to the selected class
-      elementClicked.style.borderColor = '#e5af29';
-      character.debugObject();
-    });
-  },
-  getPseudo: function() {
-    const PSEUDO_USER_INPUT = document.getElementById('pseudo-user-input');
-    const MAIN_PSEUDO = document.querySelector('h1');
-
-    PSEUDO_USER_INPUT.addEventListener('input', function(event) {
-      character.pseudo = this.value;
-      MAIN_PSEUDO.textContent = this.value;
-    });
-    character.debugObject();
+// Add quest page
+let quests = {
+  questContent: '',
+  questCategory: '',
+  completed: false,
+  xp: 50,
+  debugQuest: function() {
+    console.log(this.questContent);
+    console.log(this.questCategory);
   }
 };
 
-characterCreation.getAvatar();
-characterCreation.getClass();
-characterCreation.getPseudo();
+// Manage quests
+let questManagement = {
+  addQuest: function() {
+    const CONFIRM_ADD_QUEST = document.getElementById('confirm-add-quest');
+    const QUEST_USER_INPUT = document.getElementById('quest-user-input');
+    const ADD_QUEST_CATEGORY = document.getElementById('add-quest-category');
+
+    QUEST_USER_INPUT.addEventListener('input', function() {
+      quests.questContent = this.value;
+      quests.debugQuest();
+    });
+
+    ADD_QUEST_CATEGORY.addEventListener('click', function() {
+      quests.questCategory = 'category 1';
+      quests.debugQuest();
+    });
+
+    CONFIRM_ADD_QUEST.addEventListener('click', function(event) {
+      // debugger;
+      const CONTENT_ADD_QUEST_PAGE = document.getElementById('content-add-quest-page');
+      const FIRST_DEFAULT_QUEST = document.getElementById('first-default-quest');
+      let questElement = document.createElement('div');
+      let questElementContent = document.createElement('div');
+      let questElementCategory = document.createElement('div');
+      let questElementXP = document.createElement('div');
+      let questElementAdd = document.createElement('div');
+      let questElementAddIcon = document.createElement('i');
+      questElement.className = 'add-quest-content-default-group';
+      questElementContent.className = 'content-default-quest-disable';
+      questElementContent.textContent = quests.questContent;
+      questElementCategory.className = 'quest-category-item';
+      questElementXP.className = 'content-quest-xp-disable';
+      questElementXP.textContent = '50xp';
+      questElementAdd.className = 'confirm-add-quest-disable';
+      questElementAddIcon.className= 'fas fa-check';
+      questElementAdd.appendChild(questElementAddIcon);
+
+      CONTENT_ADD_QUEST_PAGE.insertBefore(questElement, FIRST_DEFAULT_QUEST);
+      questElement.appendChild(questElementContent);
+      questElement.appendChild(questElementCategory);
+      questElement.appendChild(questElementXP);
+      questElement.appendChild(questElementAdd);
+
+      quests.debugQuest();
+      event.preventDefault();
+    });
+  }
+};
+
+questManagement.addQuest();
