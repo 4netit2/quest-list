54bca1097126c473f8639ec97403d565cdf137db
tolisich <it14126@uom.edu.gr>
2017-12-31 18:52:12 +0200

    Changed connection file

--- backend/db.php
+++ backend/db.php
@@ -1,14 +1,14 @@
 <?php
 /* Database connection settings */
-/*$server = '127.0.0.1';
+$server = '127.0.0.1';
 $username = 'maevacongznguyen';
 $password = 'QuestList2018';
 $database_name = 'maevacongznguyen';
-$mysqli = new mysqli($server,$username,$password,$database_name) or die($mysqli->error);*/
+$mysqli = new mysqli($server,$username,$password,$database_name) or die($mysqli->error);
 
-$server = 'localhost';
+/*$server = 'localhost';
 $username = 'root';
 $password = '';
 $database_name = 'users';
-$mysqli = new mysqli($server,$username,$password,$database_name) or die($mysqli->error);
+$mysqli = new mysqli($server,$username,$password,$database_name) or die($mysqli->error);*/
 
